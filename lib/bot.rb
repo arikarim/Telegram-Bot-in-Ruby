599eece48d11adc91273c2aa40f42f41240088b4
ari karim <arikarim146@gmail.com>
2021-02-21 15:22:33 +0300

    Add bot.rb file

---
+++ lib/bot.rb
@@ -0,0 +1,26 @@
+require 'telegram/bot'
+require_relative '../token'
+require_relative 'quotes'
+class Bot
+  def initilize
+    Telegram::Bot::Client.run(TOKEN) do |bot|
+      bot.listen do |message|
+        case message.text
+        when /hi/i
+          bot.api.send_message(chat_id: message.chat.id,
+                               text: "Hello, #{message.from.first_name}, I am Ari's Chatbot! and i speak on behalf of him right now im expermintal and new")
+        when /info/i
+          bot.api.send_message(chat_id: message.chat.id,
+                               text: 'I am is 22 years old, Ihave bachelor degree in Nursing and after University i desided to join Microverse, Right now i am so happy of studying and learning')
+        when /quote/i
+          quote = Quotes.new.random_quote
+          bot.api.send_message(chat_id: message.chat.id, text: (quote['text']).to_s, date: message.date)
+          bot.api.send_message(chat_id: message.chat.id, text: 'Random joke')
+        else
+          bot.api.send_message(chat_id: message.chat.id,
+                               text: "Sorry, I don't understand '#{message.text}'. I only understand `Hi` `info` `quote`")
+        end
+      end
+    end
+  end
+end
