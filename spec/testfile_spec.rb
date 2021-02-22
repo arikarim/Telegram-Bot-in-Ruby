becc7819538aa9611e1098d4592930e0c7cefe1d
ari karim <arikarim146@gmail.com>
2021-02-22 13:28:25 +0300

    Add More tests to Rspect with Negative Tests

--- spec/testfile_spec.rb
+++ spec/testfile_spec.rb
@@ -1,5 +1,5 @@
 require_relative '../lib/quotes'
-
+require_relative '../lib/bot'
 describe Quotes do
   describe '#random_quote' do
     let(:quotes) { Quotes.new }
@@ -7,6 +7,10 @@ describe Quotes do
 
     it 'should return a hash object' do
       expect(random_quote.class).to eql(Hash)
+    end
+
+    it 'should return a hash object' do
+      expect(random_quote.class).not_to eql(Array)
     end
   end
 
@@ -18,8 +22,16 @@ describe Quotes do
       expect(json.class).to eql(Array)
     end
 
+    it 'should not return a JSON hash' do
+      expect(json.class).not_to eql(Hash)
+    end
+
     it 'returns a non-empty JSON object' do
       expect(json.length).not_to eql(0)
+    end
+
+    it 'returns a false' do
+      expect(json.length == 1).to eql(false)
     end
   end
 end
